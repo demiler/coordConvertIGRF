98be1b2eaaa599bb4306eac35611fc8f4c105f67
demiler <seerpout@gmail.com>
2021-10-30 15:12:49 +0300

    split fromGeo to 3 different functions for each program. Added sever start

--- server/launcher.js
+++ server/launcher.js
@@ -5,8 +5,8 @@ const utils = require('./utils.js');
 
 const execFile = promisify(require('child_process').execFile);
 
-const launcher = {
-  geolla: async (norad, startDT = undefined, endDT = undefined, resolution = 1) => {
+class Launcher {
+  async geolla (norad, startDT = undefined, endDT = undefined, resolution = 1) {
     const { stdout } = await execFile('./progs/geolla.py');
     const lines = stdout.trim().split('\n');
 
@@ -23,34 +23,36 @@ const launcher = {
       lla.push(out.slice(4).map(Number))
     }
     return { date, time, geo, lla };
-  },
+  }
 
-  fromGeo: async (prog, date, time, geo) => {
-    if (
-      !['geo2RDMLLGsmMltShadOnly', 'geo2BigrfOnly', 'geo2LBOnly']
-      .includes(prog)
-    ) throw `Wrong prog name - ${prog}`;
 
-    const proc = spawn(`./${prog}`, [], { cwd: './progs' });
+  *__commonGenerator (date, time, geo, ut = false) {
     if (!Array.isArray(date)) {
       date = [date];
       time = [time];
       geo = [geo];
     }
 
-    for (let i = 0; i < geo.length; i++) {
-      const curDate = date[i].split('-').join(' ')
-      if (prog === 'geo2RDMLLGsmMltShadOnly') {
-        const ut = time[i]
-          .split(':')
-          .map(Number)
-          .reduce((res, t, pow) => res + t / Math.pow(60, pow));
-        proc.stdin.write(`${ut} ${curDate} ${geo[i].join(' ')}\n`)
+    for (let i = 0; i < date.length; i++) {
+      yield {
+        date: (ut)
+          ? time[i].split(':').map(Number).reduce((r, t, pow) => r+t/Math.pow(60, pow))
+          : date[i].split('-').join(' '),
+        time: (ut)
+          ? date[i].split('-').join(' ')
+          : time[i].split(':').join(' '),
+        geo: geo[i].join(' '),
       }
-      else {
-        const tm = time[i].split(':').join(' ');
-        proc.stdin.write(`${curDate} ${tm} ${geo[i].join(' ')}\n`)
-      }
+    }
+  }
+
+
+  async __feedProgram(proc, dtgGenerator) {
+    let gened = dtgGenerator.next();
+    while (!gened.done) {
+      const { date, time, geo } = gened.value;
+      proc.stdin.write(`${date} ${time} ${geo}\n`);
+      gened = dtgGenerator.next();
     }
     proc.stdin.end();
 
@@ -61,50 +63,70 @@ const launcher = {
       ac.abort();
     });
 
-    let rawOut;
     try {
-      rawOut = await once(proc.stdout, 'data', { signal: ac.signal });
+      const raw = await once(proc.stdout, 'data', { signal: ac.signal });
+      return raw
+        .toString()
+        .split('\n')
+        .map(line => line.trim().split(/ +/))
+        .filter(String)
+        .map(vals => vals.map(Number));
     }
-    catch(err) {
-      throw errors
-    }
-
-    const values = rawOut
-      .toString()
-      .split('\n')
-      .map(line => line.trim().split(/ +/))
-      .filter(String)
-      .map(vals => vals.map(Number))
-
-    switch (prog) {
-      case 'geo2RDMLLGsmMltShadOnly':
-        const dm = [], gsm = [], mlt = [], shad = [];
-
-        for (const vals of values) {
-          dm.push(vals.slice(0, 3));
-          gsm.push(vals.slice(3, 6));
-          mlt.push(vals[6]);
-          shad.push(vals[7]);
-        }
-
-        return {
-          ...utils.explode(dm, 'dm', ['Lat', 'Lon', 'Alt']),
-          ...utils.explode(gsm, 'gsm', ['X', 'Y', 'Z']),
-          mlt, shad
-        };
-
-      case 'geo2BigrfOnly':
-        return { ...utils.explode(values, 'magn', ['X', 'Y', 'Z', 'F']) };
-
-      case 'geo2LBOnly':
-        const l = [], b = [];
-        for (const vals of values) {
-          l.push(vals[0]);
-          b.push(vals[1]);
-        }
-        return { l, b };
+    catch (abort) {
+      throw errors;
     }
   }
+
+
+  async geo2LBOnly(date, time, geo) {
+    const proc = spawn('./geo2LBOnly', [], { cwd: './progs' });
+    const generator = this.__commonGenerator(date, time, geo);
+    const values = await this.__feedProgram(proc, generator);
+
+    const l = [], b = [];
+    for (const vals of values) {
+      l.push(vals[0]);
+      b.push(vals[1]);
+    }
+
+    return { l, b };
+  }
+
+
+  async geo2RDMLLGsmMltShadOnly(date, time, geo) {
+    const proc = spawn('./geo2RDMLLGsmMltShadOnly', [], { cwd: './progs' });
+    const generator = this.__commonGenerator(date, time, geo, true);
+    const values = await this.__feedProgram(proc, generator);
+
+    const dm = [], gsm = [], mlt = [], shad = [];
+    for (const vals of values) {
+      dm.push(vals.slice(0, 3));
+      gsm.push(vals.slice(3, 6));
+      mlt.push(vals[6]);
+      shad.push(vals[7]);
+    }
+
+    return {
+      ...utils.explode(dm, 'dm', ['Lat', 'Lon', 'Alt']),
+      ...utils.explode(gsm, 'gsm', ['X', 'Y', 'Z']),
+      mlt, shad
+    };
+  }
+
+
+  async geo2BigrfOnly(date, time, geo) {
+    const proc = spawn('./geo2BigrfOnly', [], { cwd: './progs' });
+    const generator = this.__commonGenerator(date, time, geo);
+    const values = await this.__feedProgram(proc, generator);
+    return { ...utils.explode(values, 'magn', ['X', 'Y', 'Z', 'F']) };
+  }
+
+
+  async fromGeo(prog, date, time, geo) {
+    if (prog in this) return await this[prog](date, time, geo);
+    else throw 'Wrong program name';
+  }
 };
+const launcher = new Launcher();
 
 module.exports = launcher;
