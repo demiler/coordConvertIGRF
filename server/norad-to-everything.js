a38bea1230255d47f4b4ce43e71eb46e6de7e460
demiler <seerpout@gmail.com>
2022-02-04 16:50:40 +0300

    be: nte refactored to new way of working with programs

--- server/norad-to-everything.js
+++ server/norad-to-everything.js
@@ -4,7 +4,58 @@ const launcher = require('./launcher.js');
 const getters = require('./getters.js');
 const utils = require('./utils.js');
 
+const { Spawner, Geolla } = require('./apps.js');
+
 module.exports.convert = async (data) => {
+  if (!nteValidator(data)) {
+    return { code: 403, error: 'data didn\'t pass the validation' };
+  }
+
+  const glProg = new Geolla();
+  const progs = Spawner.alloc(data.filters);
+
+  const dtFrom = `${data.date[0]}T${data.time[0]}`
+  const dtTo   = `${data.date[1]}T${data.time[1]}`
+
+  progs.spawn();
+  glProg.spawn(data.norad, dtFrom, dtTo, data.step);
+
+  const geosToAdd = utils.differ(['geo.X', 'geo.Y', 'geo.Z'], data.filters);
+  const result = Object.fromEntries([ 'date', 'time', ...data.filters ].map(key => [key, []]));
+
+  try {
+    let totalLines = 0;
+    let glData = await glProg.readline();
+    while (glData !== undefined) {
+      totalLines++;
+
+      const date = glData['date'];
+      const time = glData['time'];
+      const geo = { X: glData['geo.X'], Y: glData['geo.Y'], Z: glData['geo.Z'] };
+
+      result['date'].push(date);
+      result['time'].push(glData['time']);
+      geosToAdd.forEach(geo => result[geo].push(glData[geo]));
+      progs.writeline(date, time, geo);
+
+      glData = await glProg.readline();
+    }
+    result.length = totalLines;
+
+    for (let i = 0; i < totalLines; i++) {
+      const ans = await progs.readline();
+      Object.entries(ans).forEach(([key, val]) => result[key].push(val));
+    }
+
+    progs.close();
+    glProg.close();
+    return { code: 0, data: result };
+  } catch (err) {
+    return { code: 500, error: `Something went wrong during NTE convresion ${err}` };
+  }
+}
+
+module.exports.convert_old = async (data) => {
   if (!nteValidator(data)) {
     return { code: 403, error: 'data didn\'t pass the validation' };
   }
